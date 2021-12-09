539b98bda971e42cbbbd525a81b87461ac9d3499
alexanderjfang <78180012+alexanderjfang@users.noreply.github.com>
2021-12-08 21:15:14 -0500

    commitwithgoalworking

--- app/src/main/java/saund/walkingApp/MainActivity2.kt
+++ app/src/main/java/saund/walkingApp/MainActivity2.kt
@@ -1,30 +1,71 @@
 package saund.walkingApp
 
+import android.content.ClipData
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import org.w3c.dom.Text
+import android.content.Intent
 
 
-class MainActivity2 : AppCompatActivity(), View.OnClickListener  {
+class MainActivity2 : AppCompatActivity(), View.OnClickListener {
+    //presetting stats vars to 0
+    var steps = 0
+    var calories = 0
+    var distance = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        //supportFragmentManager.beginTransaction().add(R.id.helpContainer2, helpFragment()).commit()
+        //call the goal fragment data receiver
+        receiveData()
+
+        //add buttons from xml to main
         val help_btn = findViewById<Button>(R.id.help_button)
         help_btn.setOnClickListener(this)
-        /*val close_btn = findViewById<Button>(R.id.close_button)
-        close_btn.setOnClickListener(this)*/
+        val goal_btn = findViewById<Button>(R.id.goal_button)
+        goal_btn.setOnClickListener(this)
 
+        //add text lines from xml to main
+        val stepsCounter = findViewById<TextView>(R.id.stepsToday)
+        val distanceCounter = findViewById<TextView>(R.id.distanceToday)
+        val calorieCounter = findViewById<TextView>(R.id.caloriesToday)
+
+        //preset the stats to 0
+        stepsCounter.text="Steps: " + steps
+        distanceCounter.text = "Distance: " + distance + " meters"
+        calorieCounter.text = "Approx. calories burned: " + calories
     }
 
+    //set button actions
     override fun onClick(View: View){
         if(View.id==R.id.help_button){
             supportFragmentManager.beginTransaction().replace(R.id.helpContainer1, helpFragment1()).commit()
         }
-        /*if(View.id==R.id.close_button){
-            supportFragmentManager.beginTransaction().remove(helpFragment()).commit()
-        }*/
+        if(View.id==R.id.goal_button){
+            supportFragmentManager.beginTransaction().replace(R.id.goalContainer, goalFragment()).commit()
+        }
+    }
+
+    //setting up intent function for goal fragment reception
+    internal fun receiveData(){
+        val goalTextHandler = findViewById<TextView>(R.id.goalTxt)
+        val goalProgressHandler = findViewById<TextView>(R.id.goalProgress)
+        if(intent.getStringExtra("key") != null) {
+            goalTextHandler.text = "Your goal today: " + intent.getStringExtra("key").toString().toInt() + " steps"
+            goalProgressHandler.text = "Steps until goal: " + (intent.getStringExtra("key").toString().toInt() - steps)
+        }
+    }
+
+    internal fun closeActivity(){
+        finish()
     }
 }
--- app/src/main/java/saund/walkingApp/goalFragment.kt
+++ app/src/main/java/saund/walkingApp/goalFragment.kt
@@ -1,14 +1,16 @@
 package saund.walkingApp
 
 import android.content.ClipData
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.activityViewModels
-
+import org.w3c.dom.Text
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,13 +19,15 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [helpFragment1.newInstance] factory method to
+ * Use the [goalFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class helpFragment1 : Fragment(), View.OnClickListener {
+class goalFragment : Fragment(), View.OnClickListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +35,7 @@ class helpFragment1 : Fragment(), View.OnClickListener {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
@@ -38,7 +43,7 @@ class helpFragment1 : Fragment(), View.OnClickListener {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help1, container, false)
+        return inflater.inflate(R.layout.fragment_goal, container, false)
     }
 
     companion object {
@@ -48,12 +53,12 @@ class helpFragment1 : Fragment(), View.OnClickListener {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment helpFragment.
+         * @return A new instance of fragment goalFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            helpFragment1().apply {
+            goalFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
@@ -61,16 +66,36 @@ class helpFragment1 : Fragment(), View.OnClickListener {
             }
     }
 
-    //getting button from xml
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val close_btn = getView()?.findViewById<Button>(R.id.close_button) as Button
-        close_btn.setOnClickListener(this)
+        //getting buttons from xml
+        val back_btn = getView()?.findViewById<Button>(R.id.back_button) as Button
+        back_btn.setOnClickListener(this)
+        val confirm_btn = getView()?.findViewById<Button>(R.id.confirm_button) as Button
+        confirm_btn.setOnClickListener(this)
+
     }
-    //setting button action
     override fun onClick(View: View) {
-        if(View.id==R.id.close_button){
+        //setting button actions
+        if(View.id==R.id.back_button){
             getActivity()?.supportFragmentManager?.beginTransaction()?.remove(this)?.commit();
+        }
+        if(View.id==R.id.confirm_button){
+            //saving goal here
+            val goalTyped = view?.findViewById<TextView>(R.id.goalInput)
+            val goalOkay = goalTyped?.text
+            val intent = Intent(activity, MainActivity2::class.java)
+            intent.putExtra("key", (goalOkay).toString())
+            if((goalOkay).toString().toInt() <= 10000000 && (goalOkay).toString().toInt() >= 1) {
+                //call function receive data here
+                (activity as MainActivity2).closeActivity()
+                startActivity(intent)
+                getActivity()?.supportFragmentManager?.beginTransaction()?.remove(this)?.commit();
+            }
+            else{
+                val goalTxtMsg = view?.findViewById<TextView>(R.id.enterGoal)
+                goalTxtMsg?.text = "Enter your goal in steps:\nRange: [1,10000000]"
+            }
         }
     }
 }
